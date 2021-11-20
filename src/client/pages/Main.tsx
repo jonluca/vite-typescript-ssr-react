621e0b0adba444393f8deb3edf9706c04509dfeb
JonLuca DeCaro <jdecaro@usc.edu>
2021-11-20 14:02:42 -0500

    updated versions, reset yarn lockfile

--- src/client/pages/Main.tsx
+++ src/client/pages/Main.tsx
@@ -9,7 +9,7 @@ const Main = () => {
       <div className="flex items-center flex-col pt-10">
         <h1 className="font-bold text-gray-900 text-5xl lg:text-7xl text-center ">Hi{name ? `, ${name}!` : ""}</h1>
         <h2 className={"w-2/5 p-5 items-center flex align-middle text-center min-w-[320px]"} style={{ color: "green" }}>
-          This is a Vite React SSR Tailwind boilerplate! This is new text
+          This is a Vite React SSR Tailwind boilerplate!
         </h2>
         <input
           placeholder={"Enter your name"}
