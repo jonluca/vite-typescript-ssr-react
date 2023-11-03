23b550af752a4642185359146b0b991abfe781d7
JonLuca DeCaro <jonluca.decaro@gmail.com>
2023-11-03 12:43:18 -0700

    move to yarn4, bump deps

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  printWidth: 120,
-  semi: true,
-  singleQuote: false,
-  trailingComma: "all",
-  bracketSpacing: true,
-  jsxBracketSameLine: false,
-  arrowParens: "avoid",
-};
